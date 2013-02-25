56bc97a5af3a6be54ed985701ae01613d04d0d8c
Victor Nicollet <victor@nicollet.net>
2013-02-25 20:02:23 +0100

    Remove test file

--- test.ml
+++
@@ -1,8 +0,0 @@
-let _ = 
-  let gauss_factor = 8 in
-  let gauss = Array.init (2 * gauss_factor + 1) (fun i ->
-    let m = gauss_factor in
-    let z = 3. *. float_of_int (i - m) /. float_of_int gauss_factor in
-    exp (z *. z *. -0.5) 
-  ) in
-  gauss
