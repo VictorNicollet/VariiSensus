6faf47351dbca27324c282c48b9378a8acf37539
Victor Nicollet <victor@nicollet.net>
2013-06-03 14:10:23 +0200

    ePub generation

--- Makefile
+++ Makefile
@@ -7,6 +7,24 @@ graph:
 	$(BUILD) make.native
 	./make.native --graph
 
+www/epub/OEBPS/cover.png: cover.png
+	convert cover.png -resize 600x800\> www/epub/OEBPS/cover.png
+
+www/epub/OEBPS/map.png: map-athanor.png
+	convert map-athanor.png -resize 600x800\> -size 600x800 'xc:white' +swap -gravity center -composite www/epub/OEBPS/map.png
+
+ePub : www/epub/OEBPS/map.png www/epub/OEBPS/cover.png
+	mkdir www/epub www/epub/META-INF www/epub/OEBPS || echo ''
+	$(BUILD) make.byte
+	./make.byte --ePub
+	echo "application/epub+zip" -n > www/epub/mimetype
+	cp epub/main.css www/epub/OEBPS
+	cp epub/container.xml www/epub/META-INF
+	cp epub/content.opf www/epub/OEBPS
+	cp epub/*.htm www/epub/OEBPS
+	cp epub/toc.ncx www/epub/OEBPS
+	(cd www/epub ; zip -r ../book.epub *)
+
 latex : 
 	$(BUILD) make.byte
 	./make.byte --latex
--- make.ml
+++ make.ml
@@ -397,6 +397,43 @@ let generate_latex () =
   output_string chan (inv # contents) ;
   close_out chan 
 
+let generate_epub () = 
+
+  (* OPF file (includes manifest) *) 
+  let chan = open_out "epub/content.opf" in
+  output_string chan ToEpub.opf_head ;
+  List.iter (fun (path,_) -> output_string chan
+    (Printf.sprintf "<item id=\"%s\" href=\"%s.htm\" media-type=\"application/xhtml+xml\"/>"
+       path path)) All.book_1 ;
+  output_string chan ToEpub.opf_mid ; 
+  List.iter (fun (path,_) -> output_string chan
+    (Printf.sprintf "<itemref idref=\"%s\"/>" path)) All.book_1 ;
+  output_string chan ToEpub.opf_foot ; 
+  close_out chan ;
+
+  (* NCX file (table of contents) *)
+
+  let chan = open_out "epub/toc.ncx" in
+  output_string chan ToEpub.ncx_head ;
+  List.iter (fun x -> output_string chan (ToEpub.ncx_item x)) All.book_1;
+  output_string chan ToEpub.ncx_foot ;
+  close_out chan ;
+
+  (* Generate actual chapters *) 
+
+  List.iter (fun (path, name) -> 
+    let inv = new ToEpub.toEpub in
+    inv # start_chapter name ;
+    let chan = open_in ("chapters/" ^ path) in 
+    let lexbuf = Lexing.from_channel chan in 
+    Lex.latex inv lexbuf ;
+    close_in chan ;
+    print_endline ("epub/"^path^".htm") ;
+    let chan = open_out ("epub/"^path^".htm") in
+    output_string chan (inv # contents) ;
+    close_out chan     
+  ) All.book_1 
+
 let () = 
   if Array.length Sys.argv = 1 then begin 
     generate_all () ;
@@ -406,6 +443,8 @@ let () =
     page404 () ; 
   end else if Sys.argv.(1) = "--latex" then begin 
     generate_latex () ;
+  end else if Sys.argv.(1) = "--ePub" then begin 
+    generate_epub () ;
   end else if Sys.argv.(1) = "--words" then begin
     generate_words () 
   end else if Sys.argv.(1) = "--graph" then begin 
